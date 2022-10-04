432220abdd2a0c1b5f8a5d185097b2e6d5666ebe
Magusest <redikor49@gmail.com>
2022-10-04 13:46:28 +0300

    Добавил функцию возвращающую число с плавающей точкой

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
-const getRandomNumber = (min, max) => {
-  if (min > max) {
-    let swap = min
-    min = max
-    max = swap
-  };
+// Целое случайное число в раданном диапазоне
+
+const getRangeFullNumber = (min, max) => {
+  if (min < 0 || max < 0 || min === max) {
+    return NaN;
+  } if (min > max) {
+    const swap = min;
+    min = max;
+    max = swap;
+  }
   min = Math.ceil(min);
   max = Math.floor(max);
-  let randomNumber = Math.floor(Math.random() * (max - min) + min);
+  const randomNumber = Math.floor(Math.random() * (max - min) + min);
   return randomNumber;
-}
+};
+
+getRangeFullNumber (1, 10);
+
+// Случайное число с плавающей точкой в раданном диапазоне
+
+const getRangePointNumber = (min, max, digitalAfterPoint) => {
+  if (min < 0 || max < 0 || min === max || digitalAfterPoint < 0) {
+    return NaN;
+  } if (min > max) {
+    const swap = min;
+    min = max;
+    max = swap;
+  }
+  const randomNumber = Math.random() * (max - min) + min;
+
+  return randomNumber.toFixed(digitalAfterPoint);
+};
+
+getRangePointNumber(1.2, 30.9, 3);
