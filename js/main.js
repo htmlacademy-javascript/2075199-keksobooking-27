871dc8f876a06b0619579aea6e8f51b1f16c612d
Magusest <redikor49@gmail.com>
2022-10-09 22:31:41 +0300

    Поправил функции случайных чисел

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Целое случайное число в раданном диапазоне
+// Целое случайное число в заданном диапазоне
 
 const getRangeIntegerNumber = (min, max) => {
   if (min < 0 || max < 0 || min === max) {
@@ -10,13 +10,13 @@ const getRangeIntegerNumber = (min, max) => {
   }
   min = Math.ceil(min);
   max = Math.floor(max);
-  const randomNumber = Math.floor(Math.random() * (max - min) + min);
+  const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
   return randomNumber;
 };
 
 getRangeIntegerNumber (1, 10);
 
-// Случайное число с плавающей точкой в раданном диапазоне
+// Случайное число с плавающей точкой в заданном диапазоне
 
 const getRangePointNumber = (min, max, digitalAfterPoint) => {
   if (min < 0 || max < 0 || min === max || digitalAfterPoint < 0) {
@@ -28,7 +28,7 @@ const getRangePointNumber = (min, max, digitalAfterPoint) => {
   }
   const randomNumber = Math.random() * (max - min) + min;
 
-  return randomNumber.toFixed(digitalAfterPoint);
+  return +randomNumber.toFixed(digitalAfterPoint);
 };
 
 getRangePointNumber(1.2, 30.9, 3);
