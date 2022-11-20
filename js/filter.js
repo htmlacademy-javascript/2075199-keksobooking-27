6f9bfc416f50276db68a971f105fa11f92196482
Magusest <redikor49@gmail.com>
2022-11-20 20:35:45 +0300

    Добавил фильтрацию

--- js/filter.js
+++ js/filter.js
@@ -1,4 +1,15 @@
+const OFFERS_COUNT = 10;
+const Price = {
+  MIDDLE: 10000,
+  HIGH: 50000,
+};
+
 const filtersField = document.querySelector('.map__filters');
+const housingTypeElement = filtersField.querySelector('#housing-type');
+const housingPriceElement = filtersField.querySelector('#housing-price');
+const housingRoomsElement = filtersField.querySelector('#housing-rooms');
+const housingGuestsElement = filtersField.querySelector('#housing-guests');
+const featuresCheckboxes = filtersField.querySelectorAll('.map__checkbox');
 
 const turnFiltersOff = () => {
   filtersField.classList.add('ad-form--disabled');
@@ -14,4 +25,70 @@ const turnFiltersOn = () => {
   }
 };
 
-export {turnFiltersOff, turnFiltersOn};
+const filterByType = (offer, type) => type === 'any' || offer.offer.type === type;
+
+const filterByPrice = (offer, price) => {
+  switch (price) {
+    case 'any':
+      return true;
+    case 'low':
+      return offer.offer.price < Price.MIDDLE;
+    case 'middle':
+      return offer.offer.price < Price.HIGH && offer.offer.price > Price.MIDDLE;
+    case 'high':
+      return offer.offer.price >= Price.MIDDLE;
+  }
+};
+
+const filterByRooms = (offer, rooms) => rooms === 'any' || offer.offer.rooms === +rooms;
+
+const filterByGuests = (offer, guests) => guests === 'any' || offer.offer.guests === +guests;
+
+const filterByFeatures = (offer, features) => {
+  if (!features.length) {
+    return true;
+  }
+  if (!offer.offer.features) {
+    return false;
+  }
+
+  return features.every((feature) => offer.offer.features.includes(feature));
+};
+
+const getFilteredOffers = (offers) => {
+  const selectedType = housingTypeElement.value;
+  const selectedPrice = housingPriceElement.value;
+  const selectedRooms = housingRoomsElement.value;
+  const selectedGuests = housingGuestsElement.value;
+
+  const selectedFeatures = [];
+  const filteredOffers = [];
+  featuresCheckboxes.forEach((checkbox) => {
+    if (checkbox.checked) {
+      selectedFeatures.push(checkbox.value);
+    }
+  });
+  for (const offer of offers) {
+    if (filteredOffers.length >= OFFERS_COUNT) {
+      break;
+    }
+    if (
+      filterByType(offer, selectedType) &&
+      filterByPrice(offer, selectedPrice) &&
+      filterByRooms(offer, selectedRooms) &&
+      filterByGuests(offer, selectedGuests) &&
+      filterByFeatures(offer, selectedFeatures)
+    ) {
+      filteredOffers.push(offer);
+    }
+    return filteredOffers;
+  }
+};
+
+const setOnFilterChange = (cb) => {
+  filtersField.addEventListener('change', () => {
+    cb();
+  });
+};
+
+export {turnFiltersOff, turnFiltersOn, getFilteredOffers, setOnFilterChange};
--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 import {turnAddFormOff, turnAddFormOn, setAddress, resetForm, setOnFormSubmit} from './form.js';
-import {turnFiltersOff, turnFiltersOn} from './filter.js';
+import {turnFiltersOff, turnFiltersOn, getFilteredOffers, setOnFilterChange} from './filter.js';
 import {initMap, setAdPins, setOnMainPinMove, setOnMapLoad} from './map.js';
 import {showSuccessMessage, showErrorMessage} from './message.js';
-import {showAlert} from './utils.js';
+import {showAlert, debounce} from './utils.js';
 import {getData, sendData} from './api.js';
 
 const START_COORDINATE = {
@@ -10,13 +10,18 @@ const START_COORDINATE = {
   lng: 139.73007,
 };
 
+const RERENDER_DELAY = 500;
+
 const resetCoorditate = () => {
   setAddress(START_COORDINATE);
 };
 
 const onGetDataSuccess = (offers) => {
-  setAdPins(offers);
   turnFiltersOn();
+  setAdPins(getFilteredOffers(offers));
+  setOnFilterChange(debounce(
+    () => setAdPins(getFilteredOffers(offers))
+  ), RERENDER_DELAY);
 };
 
 const onSendDataSuccess = ( ) => {
